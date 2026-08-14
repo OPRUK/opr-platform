cbb8375464e83c04a05f716547930ed55e043fee
OPRUK <info@otherpeoplesrecipes.co.uk>
2026-08-14 08:26:26 +0100

    Add butter chicken teaser to Film Collection

--- lib/films.ts
+++ lib/films.ts
@@ -22,6 +22,10 @@ function film(title: string, file: string, hasPoster = true): Film {
 
 export const films: Film[] = [
   {
+    title: "Dave & Rubble | Dave's Butter Chicken",
+    video: "/videos/opr-dave-and-rubble-daves-butter-chicken.mp4",
+  },
+  {
     title: "Dave & Rubble | Sam's Soufflé",
     video: "/videos/opr-dave-and-rubble-sams-souffle.mp4",
   },
